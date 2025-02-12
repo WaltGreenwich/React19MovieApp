769bce6ac577847184ef216cc522f8963cd21f5f
WGreenwichChaca <walter.greenwich@gmail.com>
2025-02-11 23:52:09 -0500

    Destructuring MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ movie: { title } }) => {
+  return (
+    <div>
+      <p className="text-white">{title}</p>
+    </div>
+  );
 };
 
 export default MovieCard;
